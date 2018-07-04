99ecf62a13ef4ed69af4ec576067061d13aac35c
Vaibhav <vaibhavmor@Vaibhavs-iMac.local>
2018-07-04 22:19:24 +1200

    Add functionality to allow notes to be deleted from list

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,9 @@ function eventListeners() {
 
     // Form Submission
     document.querySelector('#form').addEventListener('submit', newNote);
+
+    // Remove note from the list
+    notes.addEventListener('click', removeNote);
 }
 
 // Functions
@@ -17,10 +20,27 @@ function newNote(e) {
     // Read the textarea value
     const note = document.getElementById('tweet').value;
 
+    // Create the remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
     // Create a <li> element
     const li = document.createElement('li');
     li.textContent = note;
+
+    // Add the remove button to each note
+    li.appendChild(removeBtn);
+
+    // Add to the list
     notes.appendChild(li);
 
     console.log(note);
+}
+
+// Removes the notes from the DOM
+function removeNote(e) {
+    if(e.target.classList.contains('remove-tweet')) {
+        e.target.parentElement.remove();
+    }
 }
